316553dc56c286a756bf1a69df058a0a3168a4a4
Sam <samcope24@gmail.com>
2022-04-16 09:10:45 +0100

    Add extensions for service registration

--- Catalog/Catlog.Service/Program.cs
+++ Catalog/Catlog.Service/Program.cs
@@ -1,38 +1,14 @@
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Bson;
-using Catlog.Service.Settings;
-using MongoDB.Driver;
 using Catalog.Service.Repositories;
 using Catalog.Service.Entities;
-
-ServiceSettings serviceSettings;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers(options => options.SuppressAsyncSuffixInActionNames = false);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-builder.Services.AddSingleton(serviceProvider => {
-    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-    return mongoClient.GetDatabase(serviceSettings.ServiceName);
-});
-
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
-{
-    var database = serviceProvider.GetService<IMongoDatabase>();
-    return new MongoRepository<Item>(database, "items");
-});
-
-// Register mongodb serializers
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+builder.Services.AddMongo()
+    .AddMongoRepository<Item>("items");
 
 var app = builder.Build();
 
