2d0ceb8fa42443b62a1888afebde4080f8335b10
Sam <samcope24@gmail.com>
2022-04-17 19:04:43 +0100

    add wait and retry for requests

--- Play.Inventory/Play.Inventory.Service/Program.cs
+++ Play.Inventory/Play.Inventory.Service/Program.cs
@@ -2,8 +2,10 @@ using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
+var random = new Random();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -13,8 +15,18 @@ builder.Services.AddMongo()
     .AddMongoRepository<InventoryItem>("inventoryitems");
 
 builder.Services
-    .AddHttpClient<CatalogClient>(client =>
-        client.BaseAddress = new System.Uri("https://localhost:7148"))
+    .AddHttpClient<CatalogClient>(client =>client.BaseAddress = new System.Uri("https://localhost:7148"))
+    .AddTransientHttpErrorPolicy(policyBuilder =>
+            policyBuilder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+                retryCount: 5,
+                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                    + TimeSpan.FromMilliseconds(random.Next(0, 1000)),
+                onRetry: (outcome, timespan, retryAttempt) =>
+                {
+                    var serviceProvider = builder.Services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()?
+                        .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+                }))
     .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 var app = builder.Build();
