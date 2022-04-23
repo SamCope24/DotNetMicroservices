a7fc9a45c314c4e82f82b9c1b76fbfbe2a909245
Sam <samcope24@gmail.com>
2022-04-23 12:35:59 +0100

    add MassTransit registration extension to Play.Common

--- Play.Catalog/Play.Catalog.Service/Program.cs
+++ Play.Catalog/Play.Catalog.Service/Program.cs
@@ -1,7 +1,6 @@
 
-using MassTransit;
 using Play.Catalog.Service.Entities;
-using Play.Catalog.Service.Settings;
+using Play.Common.MassTransit;
 using Play.Common.MongoDB;
 using Play.Common.Settings;
 
@@ -14,19 +13,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddMongo()
-    .AddMongoRepository<Item>("items");
-
-builder.Services.AddMassTransit(x =>
-{
-    x.UsingRabbitMq((context, configurator) =>
-    {
-        var rabbitMQSettings = builder.Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-        configurator.Host(rabbitMQSettings.Host);
-        configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-    });
-});
-
-builder.Services.AddMassTransitHostedService();
+    .AddMongoRepository<Item>("items")
+    .AddMassTransitWithRabbitMQ();
 
 var app = builder.Build();
 
