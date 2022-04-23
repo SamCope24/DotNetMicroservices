5385adb999addee4230bdd1b0aad997860271036
Sam <samcope24@gmail.com>
2022-04-23 15:39:17 +0100

    add retries for failed message production/consumption

--- Play.Common/MassTransit/Extensions.cs
+++ Play.Common/MassTransit/Extensions.cs
@@ -21,6 +21,8 @@ namespace Play.Common.MassTransit
                     var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
                     configurator.Host(rabbitMQSettings.Host);
                     configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                    configurator.UseMessageRetry(retryConfigurator =>
+                        retryConfigurator.Interval(retryCount: 3, interval: TimeSpan.FromSeconds(5)));
                 });
             });
 
